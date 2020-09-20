fbabc72f289149db1b0f9657ce3fb744a58e0bf5
Koszany <akoszany@akamai.com>
2020-09-20 10:35:10 +0200

    Initial commit

---
+++ src/main/java/com/codecool/demo/User.java
@@ -0,0 +1,58 @@
+package com.codecool.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name can't be blank")
+    private String name;
+
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public User(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public User() {
+
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/codecool/demo/UserController.java
@@ -0,0 +1,67 @@
+package com.codecool.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import javax.validation.Valid;
+import java.util.Optional;
+import java.util.Properties;
+
+@Controller
+public class UserController {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/new")
+    String showCreateForm(User user) {
+        return "create-user";
+    }
+
+    @PostMapping("/create")
+    String createUser(@Valid User user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "create-user";
+        }
+        userRepository.save(user);
+        return "redirect:/index";
+    }
+
+
+    @GetMapping("/index")
+    String showIndex(Model model) {
+        if (userRepository.count() > 0)
+            model.addAttribute("users", userRepository.findAll());
+        else
+            model.addAttribute("users", null);
+        System.out.println(userRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/edit/{id}")
+    String showUpdateForm(@PathVariable("id") long id, Model model) {
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid id"));
+        model.addAttribute("user", user);
+        return "update-user";
+    }
+
+    @PostMapping("/update/{id}")
+    String updateUser (@PathVariable("id") long id, @Valid User user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "update-user";
+        }
+        userRepository.save(user);
+        model.addAttribute("users", userRepository.findAll());
+        return "redirect:/index";
+    }
+}
